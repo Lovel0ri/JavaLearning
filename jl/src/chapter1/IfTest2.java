e156187f68aad1c73c4010c98a137c03e34bbf3a
lishubin <2697601945@qq.com>
2022-10-15 01:29:23 +0800

    提交相关练习题p24

---
+++ jl/src/chapter1/IfTest2.java
@@ -0,0 +1,13 @@
+package chapter1;
+
+public class IfTest2 {
+    public static void main(String[] args){
+        int x = 2;
+        if (x == 3) {
+            System.out.println("X must be 3");
+        }else {
+            System.out.println("X is NOT 3");
+        }
+        System.out.println("This is runs no matter what");
+    }
+}
